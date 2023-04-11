33d4dacc0abe0ea61e6ce5c53e3ea49831e09a55
Lucas Morais Freire <lucasmoraisf@gmail.com>
2023-04-11 00:52:06 -0300

    fixed bugs in printMatrix. added python notebook

--- Simplex/simplex.py
+++ Simplex/simplex.py
@@ -9,8 +9,9 @@ def makeMatrix(z, r, min=False):
         Z.append(-z[0][i])
         if not z[1][i]:
             Z.append(z[0][i])
+    n = len(Z)
     if min:
-        z = -z
+        Z = [-i for i in Z]
     m = []
     for i in range(nr):
         m.append([])
@@ -19,11 +20,18 @@ def makeMatrix(z, r, min=False):
     for i in range(n):
         m[0][i] = Fraction(Z[i], 1)
     for i in range(1, nr):
-        for j in range(n):
+        track = 0
+        for j in range(len(z[0])):
             if r[i - 1][-2] == '>=':
-                m[i][j] = Fraction(-r[i - 1][j], 1)
+                m[i][j + track] = Fraction(-r[i - 1][j], 1)
+                if not z[1][j]:
+                    m[i][j + 1 + track] = Fraction(r[i - 1][j], 1)
+                    track += 1
             else:
-                m[i][j] = Fraction(r[i - 1][j], 1)
+                m[i][j + track] = Fraction(r[i - 1][j], 1)
+                if not z[1][j]:
+                    m[i][j + 1 + track] = Fraction(-r[i - 1][j], 1)
+                    track += 1
     for i in range(nr - 1):
         if r[i][-2] == '>=':
             m[i + 1][-1] = Fraction(-r[i][-1], 1)
@@ -34,15 +42,17 @@ def makeMatrix(z, r, min=False):
         m[i + 1][i + n] = Fraction(1, 1)
     return m
 
+def lineOperation(m, i1:int, C1:Fraction=Fraction(1,1), i2:int=0, C2:Fraction=Fraction(0,1)):
+    for j in range(len(m[0])):
+        m[i1][j] = C1*m[i1][j] + C2*m[i2][j]
+
 def makePivot(m, i, j):
     C = Fraction(1,1)/m[i][j]
-    for k in range(len(m[0])):
-        m[i][k] =C*m[i][k]
+    lineOperation(m, i, C)
     for k in range(len(m)):
         if k != i:
             C = -m[k][j]
-            for l in range(len(m[0])):
-                m[k][l] = m[k][l] + C*m[i][l]
+            lineOperation(m, k, i2=i, C2=C)
 
 def printMatrix(m):
     M = []
@@ -69,13 +79,3 @@ def printMatrix(m):
 def printQValues(m, j):
     for i in range(1, len(m)):
         print((m[i][-1]/m[i][j]).numerator/(m[i][-1]/m[i][j]).denominator)
-
-z = [[3, 2, 5], [True, True, True]]
-r = [[2, 3, 4,'<=', 10],
-     [5, 6, 2,'<=', 12]]
-
-m = makeMatrix(z, r)
-
-printMatrix(m)
-
-printQValues(m, 2)
