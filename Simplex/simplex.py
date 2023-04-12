efc26a712053ff62ec72fbd81da228b8af42d379
Lucas Morais Freire <lucasmoraisf@gmail.com>
2023-04-12 15:01:35 -0300

    acho que acabou

--- Simplex/simplex.py
+++ Simplex/simplex.py
@@ -10,7 +10,7 @@ def copyMatrix(m):
     for i in range(len(m)):
         m2.append([])
     for i in range(len(m)):
-        for j in range(len(m[0])):
+        for j in range(len(m[i])):
             m2[i].append(m[i][j])
     return m2
 
@@ -28,52 +28,72 @@ def makeMatrix(z, r, min=False):
     m = []
     for i in range(nr):
         m.append([])
-        for j in range(n + nr):
+        for j in range(n):
             m[i].append(Fraction(0,1))
     for i in range(n):
-        m[0][i] = Fraction(Z[i], 1)
+        m[0][i] = Fraction(Z[i])
+    m[0].append(Fraction(0,1))
     for i in range(1, nr):
-        track = 0
+        neg = 0
         for j in range(len(z[0])):
-            if r[i - 1][-2] == '>=':
-                m[i][j + track] = Fraction(-r[i - 1][j], 1)
+            if r[i - 1][-2] == '<=':
+                m[i][j + neg] = Fraction(r[i - 1][j], 1)
                 if not z[1][j]:
-                    m[i][j + 1 + track] = Fraction(r[i - 1][j], 1)
-                    track += 1
+                    m[i][j + 1 + neg] = Fraction(-r[i - 1][j], 1)
+                    neg += 1
+            elif r[i - 1][-2] == '>=':
+                m[i][j + neg] = Fraction(-r[i - 1][j], 1)
+                if not z[1][j]:
+                    m[i][j + 1 + neg] = Fraction(r[i - 1][j], 1)
+                    neg += 1
+            elif r[i - 1][-2] == '=':
+                if r[i - 1][-1] < 0:
+                    m[i][j + neg] = Fraction(-r[i - 1][j], 1)
+                    if not z[1][j]:
+                        m[i][j + 1 + neg] = Fraction(r[i - 1][j], 1)
+                        neg += 1
+                else:
+                    m[i][j + neg] = Fraction(r[i - 1][j], 1)
+                    if not z[1][j]:
+                        m[i][j + 1 + neg] = Fraction(-r[i - 1][j], 1)
+                        neg += 1
+        if r[i - 1][-2] == '<=':
+            m[i].append(Fraction(r[i - 1][-1], 1))
+        elif r[i - 1][-2] == '>=':
+            m[i].append(Fraction(-r[i - 1][-1], 1))
+        elif r[i - 1][-2] == '=':
+            if r[i - 1][-1] < 0:
+                m[i].append(Fraction(-r[i - 1][-1], 1))
             else:
-                m[i][j + track] = Fraction(r[i - 1][j], 1)
-                if not z[1][j]:
-                    m[i][j + 1 + track] = Fraction(-r[i - 1][j], 1)
-                    track += 1
-    for i in range(nr - 1):
-        if r[i][-2] == '>=':
-            m[i + 1][-1] = Fraction(-r[i][-1], 1)
-        else:
-            m[i + 1][-1] = Fraction(r[i][-1], 1)
-        
-    for i in range(nr - 1):
-        m[i + 1][i + n] = Fraction(1, 1)
-    art = 0
-    for i in range(nr):
+                m[i].append(Fraction(r[i - 1][-1], 1))
+    for i in range(1, nr):
+        if r[i - 1][-2] != '=':
+            m[0].append(Fraction(0,1))
+            for j in range(1, nr):
+                if j == i:
+                    m[j].insert(-1, Fraction(1,1))
+                else:
+                    m[j].insert(-1, Fraction(0,1))
+    twoStep = False
+    for i in range(1, nr):
         if m[i][-1] < 0:
-            art += 1
-    if art != 0:
-        for i in range(nr):
-            for j in range(art):
-                m[i].insert(- 1, Fraction(0, 1))
-        art = 0
-        for i in range(nr):
-            if m[i][-1] < 0:
-                m[i][n + nr + art - 1] = Fraction(-1, 1)
-                art += 1
-        for i in range(len(m[0])):
-            m[0][i] = Fraction(0, 1) if (i < n + nr - 1 or i == len(m[0]) - 1) else Fraction(1,1)
+            twoStep = True
+            m[0].insert(-1, Fraction(1,1))
+            for j in range(1, nr):
+                if j == i:
+                    m[j].insert(-1, Fraction(-1, 1))
+                else:
+                    m[j].insert(-1, Fraction(0, 1))
+    if twoStep:
+        for i in range(n):
+            m[0][i] = Fraction(0,1)
     return m
 
 def lineOperation(m, i1:int, C1:Fraction=Fraction(1,1), i2:int=0, C2:Fraction=Fraction(0,1)):
     c1 = C1 if C1.denominator == 1 else (f'{"-" if C1 < 0 else ""}' + '\\frac{' + str(abs(C1.numerator)) + '}{' + str(C1.denominator) + '}')
-    c2 = C2 if C2.denominator == 1 else (f'{"-" if C1 < 0 else ""}' + '\\frac{' + str(abs(C2.numerator)) + '}{' + str(C2.denominator) + '}')
-    img = formula_as_file('L_{' + str(i1) + '} \\rightarrow ' + f'{c1 if C1 != 1 and C1 != -1 else ("-" if C1 == -1 else "")}' + 'L_{' + str(i1) + '}' + f'{"+" if C2 > 0 else ("-" if C2 < 0 else "")} {c2 if (C2 > 0 and C2 != 1) else (str(c2) if (C2 < 0 and C2 != -1) else "")}' + f'{"L_{" if C2 != 0 else ""}' + f'{i2 if C2 != 0 else ""}' + f'{"}" if C2 != 0 else ""}')
+    c2 = C2 if C2.denominator == 1 else (f'{"-" if C2 < 0 else ""}' + '\\frac{' + str(abs(C2.numerator)) + '}{' + str(C2.denominator) + '}')
+    #img = formula_as_file('L_{' + str(i1) + '} \\rightarrow ' + f'{c1 if C1 != 1 and C1 != -1 else ("-" if C1 == -1 else "")}' + 'L_{' + str(i1) + '}' + f'{"+" if C2 > 0 else ""} {c2 if (C2 > 0 and C2 != 1) else (str(c2) if (C2 < 0 and C2 != -1) else "")}' + f'{"L_{" if C2 != 0 else ""}' + f'{i2 if C2 != 0 else ""}' + f'{"}" if C2 != 0 else ""}')
+    img = formula_as_file(f'L_{"{"}{str(i1)}{"}"} \\rightarrow ' + ('' if C1 == 1 else ('-' if C1 == -1 else str(c1))) + f'L_{"{"}{str(i1)}{"}"}' + ('+' if C2 > 0 else '') + ('' if C2 == 1 or C2 == 0 else ('-' if C2 == -1 else str(c2))) + (f'L_{"{"}{str(i2)}{"}"}' if C2 != 0 else ''))
     display(img)
     for j in range(len(m[0])):
         m[i1][j] = C1*m[i1][j] + C2*m[i2][j]
@@ -141,14 +161,25 @@ def printQValues(m, j):
     for i in range(1, len(m)):
         print((m[i][-1]/m[i][j]).numerator/(m[i][-1]/m[i][j]).denominator)
 
-def putBack(m:list[list[Fraction]], z, min=False):
-    Z = []
-    n = len(z[0])
-    for i in range(n):
-        Z.append(-z[0][i])
-        if not z[1][i]:
-            Z.append(z[0][i])
-    for i in range(len(Z)):
-        m[0][i] = Fraction(Z[i], 1)
-    for i in range(len(Z), len(m[0])):
-        m[0][i] = Fraction(0,1)
+def putBack(m:list[list[Fraction]], z, r, min=False):
+    r2 = copyMatrix(r)
+    for row in r2:
+        if row[-2] == "<=" and row[-1] < 0:
+            row[-1] = -row[-1]
+        elif row[-2] == ">=" and row[-1] > 0:
+            row[-1] = -row[-1]
+        elif row[-2] == '=' and row[-1] < 0:
+            row[-1] = -row[-1]
+    m2 = makeMatrix(z, r2, min)
+    for i in range(1, len(m2)):
+        for j in range(len(m2[i]) - 1):
+            m2[i][j] = m[i][j]
+    for i in range(1, len(m2)):
+        m2[i][-1] = m[i][-1]
+    return m2
+
+# z = [[7, 9], [True, True]]
+# r = [[1, -1, '>=', -2],
+#      [3, 5, '>=', 15],
+#      [5, 4, '>=', 20]]
+# m = makeMatrix(z, r)
--- Simplex/test.py
+++
@@ -1,5 +0,0 @@
-from fractions import *
-
-L = [1,2,3,4]
-L.insert(-1,0)
-print(L)
